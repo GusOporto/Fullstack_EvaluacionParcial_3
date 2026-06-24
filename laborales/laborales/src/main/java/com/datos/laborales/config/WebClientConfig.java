e8ad896e3bc84b6b9e5fff306c97427c52c06d5b
17kjaja <be.saavedrac@duocuc.cl>
2026-06-24 17:14:40 -0400

    Cambios en .Yml y cambios en WebConfig
    Se cambiaron cosas en WebClientConfig que se solicitan para evitar errores y se agregan cambios en .yml para que funcione el servicio con el mio de laborales

---
+++ laborales/laborales/src/main/java/com/datos/laborales/config/WebClientConfig.java
@@ -0,0 +1,31 @@
+package com.datos.laborales.config;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class WebClientConfig {
+
+    @Bean
+    @LoadBalanced
+    public WebClient.Builder webClientBuilder(){
+        return WebClient.builder();
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOriginPatterns("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+                        .allowedHeaders("*")            
+                        .allowCredentials(false);   
+            }
+        };
+    }
+
+}
