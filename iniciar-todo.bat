5da0d10c31fb83f92ac5386e31e64ff0eb03a7f4
GusOporto <gu.oporto@duocuc.cl>
2026-06-28 20:58:28 -0400

    Cambios en iniciar-todo y gateway
    Se configura nuevamente iniciar-todo.
    Se configura Gateway para implementación con microservicio ColaboradoresSucursales.

--- iniciar-todo.bat
+++ iniciar-todo.bat
@@ -5,14 +5,19 @@ cd eureka
 start cmd /k "mvnw spring-boot:run"
 
 echo Esperando 12 segundos a que Eureka se estabilice...
-timeout /t 12 /nobreak > null
+timeout /t 12 /nobreak > nul
 
 echo Iniciando API Gateway...
 cd ../gateway
 start cmd /k "mvnw spring-boot:run"
 
+echo Esperando 5 segundos...
+timeout /t 5 /nobreak > nul
+
 echo Iniciando Microservicio ColaboradoresSucursales...
 cd ../ColaboradoresSucursales
 start cmd /k "mvnw spring-boot:run"
 
-echo Ecosistema lanzado. Dashboard disponible en http://localhost:8761
+echo Ecosistema lanzado. 
+echo - Dashboard disponible en http://localhost:8761
+echo - Gateway / Swagger: http://localhost:8080/swagger-ui/index.html
